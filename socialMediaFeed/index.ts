ef45acc24ea7cbbd1c1bde32afea61457fed3777
danmarksteiner <marksteiner.dan@gmail.com>
2020-07-08 16:52:13 +0100

    Genrate TS config file

--- socialMediaFeed/index.ts
+++ socialMediaFeed/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+// Start Express Server
 const app = express();
 
+// JSON parsing middleware
 app.use(json());
 
+// Listen on port 3000
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
